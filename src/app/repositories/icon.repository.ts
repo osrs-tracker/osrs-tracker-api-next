e5c738c212125c862980457aa0d80fa3fb7bbf9f
Freek Mencke <freek_mencke@outlook.com>
2019-08-22 18:14:39 +0200

    Clean icon repo promises (#11)
    * Clean icon repo promises
    
    * removed unused imports

--- src/app/repositories/icon.repository.ts
+++ src/app/repositories/icon.repository.ts
@@ -1,8 +1,9 @@
 
 import Axios from 'axios';
-import { createWriteStream, exists, mkdir, readFile, stat, Stats, unlinkSync } from 'fs';
+import { createWriteStream, exists, mkdir, readFile, stat, unlink } from 'fs';
 import { join } from 'path';
 import { Stream } from 'stream';
+import { promisify } from 'util';
 import { Logger } from '../common/logger';
 import { ProxyRepository } from '../repositories/proxy.repository';
 
@@ -11,46 +12,37 @@ export class IconRepository {
   static async getIcon(id: string): Promise<Buffer | null> {
     const iconFolderPath = join(__dirname, '/icons/');
     const iconPath = join(iconFolderPath, `${id}.gif`);
+
     try {
       await this.verifyFolder(iconFolderPath);
       await this.verifyIcon(iconPath);
-      return await this.getIconFile(iconPath);
     } catch (e) {
       const iconUrl = await this.getIconUrl(id);
-      if (!iconUrl) {
-        return null;
-      }
+      if (!iconUrl) return null;
+
       Logger.logTask('ICON_REPOSITORY', `DOWNLOADING ICON FOR ITEM ${id}`);
       await this.downloadIcon(iconPath, iconUrl);
-      return this.getIconFile(iconPath);
     }
-
+    return promisify(readFile)(iconPath);
   }
 
   /** Verifies if folder exists, creates it if it doesn't. */
   private static async verifyFolder(path: string): Promise<void> {
-    const available = await new Promise<boolean>(resolve => exists(path, (exists) => resolve(exists)));
+    const available = await promisify(exists)(path);
 
     if (!available) {
-      return new Promise((resolve, reject) => mkdir(path, (err) => err ? reject(err) : resolve()));
+      return promisify(mkdir)(path);
     }
   }
 
   /** Verifies if the icon exists, and will delete it if it has a size of 0. */
   private static async verifyIcon(path: string): Promise<void> {
-    const iconStat = await new Promise<Stats>((resolve, reject) =>
-      stat(path, (err, stats) => err ? reject(err) : resolve(stats)),
-    );
-    if (iconStat.size === 0) {
-      unlinkSync(path);
-    }
-  }
+    const stats = await promisify(stat)(path);
 
-  /** Read the previously downloaded icon file from the icon folder. */
-  private static getIconFile(path: string): Promise<Buffer> {
-    return new Promise((resolve, reject) =>
-      readFile(path, (err, data) => err ? reject(err) : resolve(data)),
-    );
+    if (stats.size === 0) {
+      await promisify(unlink)(path);
+      throw new Error('FILE WITH SIZE 0');
+    }
   }
 
   /** Fetches the item and retrieves the icon url from the response. */
